cc10eae3d0fcb3e2b447283d5db74bb6fa98c587
CzDani42 <109271376+CzDani42@users.noreply.github.com>
2023-02-10 20:20:14 +0100

    Created ElementCodes array and Atom class

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,159 @@
+#include <vector>
 
+const std::string ElementCodes[] = {
+    "H",
+    "He",
+    "Li",
+    "Be",
+    "B",
+    "C",
+    "N",
+    "O",
+    "F",
+    "Ne",
+    "Na",
+    "Mg",
+    "Al",
+    "Si",
+    "P",
+    "S",
+    "Cl",
+    "Ar",
+    "K",
+    "Ca",
+    "Sc",
+    "Ti",
+    "V",
+    "Cr",
+    "Mn",
+    "Fe",
+    "Co",
+    "Ni",
+    "Cu",
+    "Zn",
+    "Ga",
+    "Ge",
+    "As",
+    "Se",
+    "Br",
+    "Kr",
+    "Rb",
+    "Sr",
+    "Y",
+    "Zr",
+    "Nb",
+    "Mo",
+    "Tc",
+    "Ru",
+    "Rh",
+    "Pd",
+    "Ag",
+    "Cd",
+    "In",
+    "Sn",
+    "Sb",
+    "Te",
+    "I",
+    "Xe",
+    "Cs",
+    "Ba",
+    "La",
+    "Ce",
+    "Pr",
+    "Nd",
+    "Pm",
+    "Sm",
+    "Eu",
+    "Gd",
+    "Tb",
+    "Dy",
+    "Ho",
+    "Er",
+    "Tm",
+    "Yb",
+    "Lu",
+    "Hf",
+    "Ta",
+    "W",
+    "Re",
+    "Os",
+    "Ir",
+    "Pt",
+    "Au",
+    "Hg",
+    "Tl",
+    "Pb",
+    "Bi",
+    "Po",
+    "At",
+    "Rn",
+    "Fr",
+    "Ra",
+    "Ac",
+    "Th",
+    "Pa",
+    "U",
+    "Np",
+    "Pu",
+    "Am",
+    "Cm",
+    "Bk",
+    "Cf",
+    "Es",
+    "Fm",
+    "Md",
+    "No",
+    "Lr",
+    "Rf",
+    "Db",
+    "Sg",
+    "Bh",
+    "Hs",
+    "Mt",
+    "Ds",
+    "Rg",
+    "Cn",
+    "Nh",
+    "Fl",
+    "Mc",
+    "Lv",
+    "Ts",
+    "Og"
+}
+
+
+
+class Atom
+{
+private:
+    int proton, neutron, electron;
+public:
+    Atom(int inpPro, int inpNeu, int inpEle)
+    {
+        proton = inpProton;
+        neutron = inpNeu;
+        electron = inpEle;
+    }
+    Atom(int inpPro, int inpNeu, int inpEle)
+    {
+        proton = inpProton;
+        neutron = inpNeu;
+        electron = inpEle;
+    }
+    int GetMass()
+    {
+        return proton + neutron;
+    }
+    int GetNeutronCount()
+    {
+        return neutron;
+    }
+    int GetNumber()
+    {
+        return proton;
+    }
+    std::string GetCode()
+    {
+        return ElementCodes[proton];
+    }
+};
